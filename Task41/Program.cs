774717bf59e580aa328e369b54b60367dfe9d53e
Andrei Krylov <dronovoy@yandex.ru>
2023-01-31 22:33:01 +0300

    Homework is done

---
+++ Task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите число - длину массива");
+if(!int.TryParse(Console.ReadLine(), out int n) || n < 0)
+{
+Console.WriteLine("Введено нецелое или отрицательное число");
+return;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Введите число");
+        if(!int.TryParse(Console.ReadLine(), out int A))
+        {
+            Console.WriteLine("Введено нецелое число");
+            return;
+        }
+        array[i] = A;
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine("Внимание, ваш массив:");
+PrintArray(array);
+
+int FindPositive(int[] array)
+{
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (array[i] > 0) count++;
+}
+return count;
+}
+
+Console.WriteLine();
+int result = FindPositive(array);
+Console.WriteLine($"Количество положительных чисел в массиве равно {result}");
---
+++ Task43/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Будем искать точку пересечения прямых, заданных в формате y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Введите число k1");
+if(!int.TryParse(Console.ReadLine(), out int k1))
+{
+    Console.WriteLine("Введено нецелое число");
+}
+Console.WriteLine("Введите число b1");
+if(!int.TryParse(Console.ReadLine(), out int b1))
+{
+    Console.WriteLine("Введено нецелое число");
+    return; 
+}
+Console.WriteLine("Введите число k2");
+if(!int.TryParse(Console.ReadLine(), out int k2))
+{
+    Console.WriteLine("Введено нецелое число");
+}
+Console.WriteLine("Введите число b2");
+if(!int.TryParse(Console.ReadLine(), out int b2))
+{
+    Console.WriteLine("Введено нецелое число");
+    return; 
+}
+
+if (k1 == k2)
+{
+    Console.WriteLine("Параллельные прямые не пересекаются, доказано Эвклидом");
+    return;
+}
+
+double[] FindIntersection (double k1, double b1, double k2, double b2)
+{
+double[] result = new double [2];
+result[0] = ((b2-b1) / (k1-k2));
+result[1] = result[0]*k1 + b1;
+return result;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+double[] array = FindIntersection(k1, b1, k2, b2);
+Console.WriteLine("Внимание, координаты точки пересечения:");
+PrintArray(array);
+
+
+
+
